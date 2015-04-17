1eced6dcb6aef157323b73e399e74d7827c7754b
Kenneth Prado <Kennethprado45@gmail.com>
2015-04-16 19:56:39 -0700

    To-Do List Video 5 code. Error found in this video.

--- includes/connect.php
+++ includes/connect.php
@@ -2,12 +2,12 @@
 //Why is this important? Why does localhost have to be first?
 $mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
 //If mysqli->connect_error then we want it to die and have this message
-if($mysqli->connect_error) {
-	die('Connect Error(' . $mysqli->connect_error . ')'
+if ($mysqli->connect_error) {
+	die('Connect Error (' . $mysqli->connect_errno . ')'
 	. $mysqli->connect_error);
 }
 else {
-	echo "Connect made";
+	echo "Connection made";
 }
 $mysqli->close();
 
