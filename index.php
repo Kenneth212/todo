3f4b39afa184d1c64638accfd288ea648fc242f5
Kenneth Prado <Kennethprado45@gmail.com>
2015-04-28 19:52:16 -0700

    Finished with the todo2 css.

--- index.php
+++ index.php
@@ -3,24 +3,26 @@
 <head>
 	<title>Kenneth's To-Do List</title>
 	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
 </head>
 <body>
 	<div class="wrap">
 		<div class="task-list">
 			<ul>
 				<?php require("includes/connect.php");
-				$mysqli() = new mysqli('localhost', 'root', 'root', 'todo');
+				$mysqli = new mysqli('localhost', 'root', 'root', 'todo');
 				$query = "SELECT = FROM tasks ORDER BY date ASC, time ASC";
 				if ($result = $mysqli->query($query)) {
 					$numrows = $result->num_rows;
 					if ($numrows>0) {
 						while($row = $result->fetch_assoc()){
-							$task_id = row('id');
-							$task_name = $row("task");
+							$task_id = $row['id'];
+							$task_name = $row["task"];
 
 							echo '<li>
 							<span>'.$task_name. '</span>
-							<img id="'.$task_id.'" class="delete-button" width="10px" src"images/close.ivg"/>
+							<img id="'.$task_id.'" class="delete-button" width="10px" src="images/close.svg"/>
 							</li>';
 						}
 					}
@@ -33,13 +35,14 @@
 	</form>
 	</div>
 </body>
-<script src="http://code.jquery-latest.min.js"></script>
+<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script>
 		add_task(); //calling the add task function
 
 		function add_task(){
 			$('.add-new-task').submit(function() {
-				var new_task = $('.add-new-task input[name=new-task').val();
+				var new_task = $('.add-new-task input[name=new-task]').val();
 
 				if (new_task != '') {
 					$.post('include/add-task.php', {task: new_task}, function(data) {
@@ -55,12 +58,11 @@
 			var current_element = $(this);
 			var task_id = $(this).attr('id');
 
-			$.post('includes/delete-task.php'. {id: task_id}. function(){
+			$.post('includes/delete-task.php', {id: task_id}, function(){
 			current_element.parent().fadeOut("fast", function(){
 				$(this).remove();
 			});
 		});
 	});
 </script>
-
 </html>
